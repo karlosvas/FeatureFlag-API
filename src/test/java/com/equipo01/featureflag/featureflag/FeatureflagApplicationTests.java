60acbcfa8a713d77b3fc4ccec397b7ee81de1e07
karlosvas <carlosvassan@gmail.com>
2025-09-09 16:58:49 +0200

    chore: update github actions secrets and add javadoc

--- src/test/java/com/equipo01/featureflag/featureflag/FeatureflagApplicationTests.java
+++ src/test/java/com/equipo01/featureflag/featureflag/FeatureflagApplicationTests.java
@@ -3,6 +3,7 @@ package com.equipo01.featureflag.featureflag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 /**
  * Pruebas de integración para la aplicación Feature Flag.
